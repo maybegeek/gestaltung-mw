0a44e985c8df8cca71a82095e5b1686942f174af
cat-mint <christine.fraunhofer@ur.de>
2019-07-05 10:40:38 +0200

    added windows makefile

---
+++ makefile-win.bat
@@ -0,0 +1,10 @@
+SETLOCAL
+SET "sourcedir=%cd%"
+PUSHD "%sourcedir%"
+FOR /f "delims=" %%a IN (
+ 'dir /b /s /a-d *mwmd.md '
+ ) DO (
+pandoc "%%a" -f markdown --pdf-engine=xelatex --template=layout/mwmd.tex layout/mwmd.yaml --filter pandoc-citeproc -o "%%~dpna.pdf"
+)
+popd
+GOTO :EOF
